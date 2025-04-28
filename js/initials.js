74381f0772a07d3f2393037475e9df02b78766de
ludvig-ekstrom <eklu24jd@student.ju.se>
2025-04-28 13:58:26 +0200

    Js code and styling

--- js/initials.js
+++ js/initials.js
@@ -1,3 +1,23 @@
 
     document.addEventListener('DOMContentLoaded', function() {
-    const f
+    const fullName="Erik Svensson"; 
+
+    function getInitials(name) {
+        const namesArray=name.trim().split(" ");
+        const initials=namesArray.map(word => word[0].toUpperCase()).join("");
+        return initials;
+    }
+    
+    const profileCircle=document.getElementById("profileCircle");
+    const mainCircle=document.getElementById("mainCircle");
+
+    if (profileCircl) {
+        profileCircle.textContent = getInitials(fullName);
+    }
+
+    if (mainCircle) {
+        mainCircle.textContent = getInitials(fullName);
+    }
+
+    profileCircle.textContent = getInitials(fullName);
+    mainCircle.textContent = getInitials(fullName);
