c702b3fb25ed8288525d82d9d0591aacf656b43d
ludvig-ekstrom <eklu24jd@student.ju.se>
2025-05-05 10:23:45 +0200

    Styling and creating js for initialas

--- js/initials.js
+++ js/initials.js
@@ -1,23 +1,3 @@
 
     document.addEventListener('DOMContentLoaded', function() {
-    const fullName="Erik Svensson"; 
-
-    function getInitials(name) {
-        const namesArray=name.trim().split(" ");
-        const initials=namesArray.map(word => word[0].toUpperCase()).join("");
-        return initials;
-    }
-    
-    const profileCircle=document.getElementById("profileCircle");
-    const mainCircle=document.getElementById("mainCircle");
-
-    if (profileCircl) {
-        profileCircle.textContent = getInitials(fullName);
-    }
-
-    if (mainCircle) {
-        mainCircle.textContent = getInitials(fullName);
-    }
-
-    profileCircle.textContent = getInitials(fullName);
-    mainCircle.textContent = getInitials(fullName);
+    const f
