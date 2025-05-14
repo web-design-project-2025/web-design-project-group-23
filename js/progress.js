a9043fec7549ca11d39cb43a8f7e1c709a21b954
ludvig-ekstrom <eklu24jd@student.ju.se>
2025-05-14 17:27:08 +0200

    xp bar

--- js/progress.js
+++ js/progress.js
@@ -1,3 +1,15 @@
-.skill {
-    width
-}
+/* XP */
+document.addEventListener("DOMContentLoaded", () => {
+  const profile = JSON.parse(localStorage.getItem("userProfile"));
+  if (profile && typeof profile.xp === "number") {
+    const xp = profile.xp;
+    const maxXp = 100;
+    const percentage = (xp / maxXp) * 100;
+
+    const xpBar = document.getElementById("xp-bar");
+    const xpTest = document.getElementById("xp-text");
+
+    xpBar.style.width = `${percentage}%`;
+    xpText.textContent = `${xp} / ${maxXP} XP`;
+  }
+});
