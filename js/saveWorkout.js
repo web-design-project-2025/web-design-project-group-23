8ee83fb87bc3a6d9d385f00b50128ca2c5107138
LopasReng <galo24bj@student.ju.se>
2025-05-14 18:15:05 +0200

    Fixing responsiveness on the planner page

--- js/saveWorkout.js
+++ js/saveWorkout.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
   if (dateDisplay && selectedDate) {
     dateDisplay.textContent = `Workout: ${selectedDate}`;
   }
+  });
+
 
   const confirmButton = document.getElementById("confirmButton");
   confirmButton.addEventListener("click", () => {
@@ -46,4 +48,3 @@ document.addEventListener("DOMContentLoaded", () => {
     alert("Workout Saved!");
     window.location.href = "calendar.html";
   });
-});
