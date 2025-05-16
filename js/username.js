ecb4d45a7a8bfb9fe9c208c83ec16a5fe8468486
BEN <gube24bx@student.ju.se>
2025-05-16 14:28:04 +0200

    username Display

--- js/username.js
+++ js/username.js
@@ -1,1 +1,14 @@
- 
+document.addEventListener("DOMContentLoaded", () => {
+  const profile = JSON.parse(localStorage.getItem("userProfile"));
+
+  if (profile && typeof profile.name === "string") {
+    const usernameDisplay = document.getElementById("#usernameDisplay");
+
+  }
+
+  if (usernameDisplay) {
+
+usernameDisplay.textContent = profile.name;
+
+  }
+});
